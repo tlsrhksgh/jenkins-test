d16955623be442f7e6b880dedee3b2a5e3019383
tlsrhksgh <sin99145@naver.com>
2023-05-25 20:59:39 +0900

    docker build url 수정

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+app.get("/hello-node", (req, res) => {
+  res.send("hello node!!")
+})
+
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
